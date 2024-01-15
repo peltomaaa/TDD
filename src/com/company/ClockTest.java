84ab57506837d0135d47468a3c51e70a6d177714
Ademir Zjajo <75216401+AdemirZjajo@users.noreply.github.com>
2024-01-15 23:21:45 +0100

    varje testfall/metod ska enbart använda en enda assertEquals(...)

--- src/com/company/ClockTest.java
+++ src/com/company/ClockTest.java
@@ -31,12 +31,31 @@ class ClockTest {
     void timeBoundaryNegative() {
         // Check negative values
         clockTester.ready();
-        expectedOutput = "Time set to: Error! Negative value";
-        assertEquals(expectedOutput,  clockTester.set(-1,1,1));
+        assertEquals(expectedOutput,  clockTester.set(1,1,-1));
+        /** PASS **/
+    }
+    @Test
+    void timeBoundaryNegative() {
+        // Check negative values
         clockTester.ready();
         assertEquals(expectedOutput,  clockTester.set(1,-1,1));
+        /** PASS **/
+    }
+    @Test
+    void timeBoundaryNegative() {
+        // Check negative values
         clockTester.ready();
-        assertEquals(expectedOutput,  clockTester.set(1,1,-1));
+        expectedOutput = "Time set to: Error! Negative value";
+        assertEquals(expectedOutput,  clockTester.set(-1,1,1));
+        /** PASS **/
+    }
+//
+    @Test
+    void timeBoundaryToHigh() {
+        // Check to high values
+        clockTester.ready();
+        expectedOutput = "Time set to: Error! Hour can not be higher then 24";
+        assertEquals(expectedOutput,  clockTester.set(25,1,1));
         /** PASS **/
     }
 
@@ -44,82 +63,125 @@ class ClockTest {
     void timeBoundaryToHigh() {
         // Check to high values
         clockTester.ready();
-        expectedOutput = "Time set to: Error! Hour can not be higher then 24";
-        assertEquals(expectedOutput,  clockTester.set(25,1,1));
-        clockTester.ready();
         expectedOutput  = "Time set to: Error! Minute can not be higher then 59";
         assertEquals(expectedOutput,  clockTester.set(1,60,1));
+        /** PASS **/
+    }
+
+    @Test
+    void timeBoundaryToHigh() {
+        // Check to high values
         clockTester.ready();
         expectedOutput  = "Time set to: Error! Second can not be higher then 59";
         assertEquals(expectedOutput,  clockTester.set(1,1,60));
         /** PASS **/
     }
-
+//
     @Test
     void timeBoundaryCloseToLimit() {
         // Check to high values
-        clockTester.ready();
-        expectedOutput = "Time set to: Hour: 23 Minute: 1 Second: 1";
-        assertEquals(expectedOutput,  clockTester.set(23,1,1));
-        clockTester.ready();
-        expectedOutput  = "Time set to: Hour: 1 Minute: 59 Second: 1";
-        assertEquals(expectedOutput,  clockTester.set(1,59,1));
         clockTester.ready();
         expectedOutput  = "Time set to: Hour: 1 Minute: 1 Second: 59";
         assertEquals(expectedOutput,  clockTester.set(1,1,59));
         /** PASS **/
     }
+    @Test
+    void timeBoundaryCloseToLimit() {
+        // Check to high values
+        clockTester.ready();
+        expectedOutput  = "Time set to: Hour: 1 Minute: 59 Second: 1";
+        assertEquals(expectedOutput,  clockTester.set(1,59,1));
 
+        /** PASS **/
+    }  @Test
+    void timeBoundaryCloseToLimit() {
+        // Check to high values
+        clockTester.ready();
+        expectedOutput = "Time set to: Hour: 23 Minute: 1 Second: 1";
+        assertEquals(expectedOutput,  clockTester.set(23,1,1));
+        /** PASS **/
+    }
+    //
     @Test
     void dateBoundaryZero() {
         // Check zero values
         clockTester.ready();
         expectedOutput = "Date set to: Error! Negative or Zero value";
         assertEquals(expectedOutput,  clockTester.set(1,1,0));
-        clockTester.ready();
-        assertEquals(expectedOutput,  clockTester.set(1,0,1));
+    }
+
+    @Test
+    void dateBoundaryZero() {
+        // Check zero values
         clockTester.ready();
         assertEquals(expectedOutput,  clockTester.set(0,1,1));
         /** PASS **/
     }
-
+    @Test
+    void dateBoundaryZero() {
+        // Check zero values
+        clockTester.ready();
+        assertEquals(expectedOutput,  clockTester.set(1,0,1));
+        /** PASS **/
+    }
+//
+    @Test
+    void dateBoundaryNegative() {
+        // Check zero values
+        clockTester.ready();
+        assertEquals(expectedOutput,  clockTester.set(-1,1,1));
+        /** PASS **/
+    }
     @Test
     void dateBoundaryNegative() {
         // Check zero values
         clockTester.ready();
         expectedOutput = "Date set to: Error! Negative or Zero value";
         assertEquals(expectedOutput,  clockTester.set(1,1,-1));
-        clockTester.ready();
-        assertEquals(expectedOutput,  clockTester.set(1,-1,1));
-        clockTester.ready();
-        assertEquals(expectedOutput,  clockTester.set(-1,1,1));
         /** PASS **/
     }
-
+    @Test
+    void dateBoundaryNegative() {
+        // Check zero values
+        clockTester.ready();
+        assertEquals(expectedOutput,  clockTester.set(1,-1,1));
+        /** PASS **/
+    }
+//
     @Test
     void dateBoundaryToHigh() {
         // Check to high values
         clockTester.ready();
         expectedOutput  = "Date set to: Error! Month can not be higher then 12";
         assertEquals(expectedOutput,  clockTester.set(2024,13,1));
+        /** PASS **/
+    }
+    @Test
+    void dateBoundaryToHigh() {
+        // Check to high values
         clockTester.ready();
         expectedOutput  = "Date set to: Error! Day can not be higher then 31";
         assertEquals(expectedOutput,  clockTester.set(2024,1,32));
         /** PASS **/
     }
-
+//
     @Test
     void dateBoundaryCloseToLimit() {
         // Check to high values
         clockTester.ready();
         expectedOutput  = "Date set to: Year: 2024 Month: 12 Day: 1";
         assertEquals(expectedOutput,  clockTester.set(2024,12,1));
+        /** PASS **/
+    }
+    @Test
+    void dateBoundaryCloseToLimit() {
+        // Check to high values
         clockTester.ready();
         expectedOutput  = "Date set to: Year: 2024 Month: 1 Day: 31";
         assertEquals(expectedOutput,  clockTester.set(2024,1,31));
         /** PASS **/
     }
-
+//
     @Test
     void transition_1_3_1() {
         clockTester.ready();
@@ -127,63 +189,109 @@ class ClockTest {
         clockTester.ready();
         expectedOutput = "Time ready to be set!";
         assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
+    @Test
+    void transition_1_3_1() {
+        clockTester.ready();
+        // Check Transition-1-3-1, From Display_Time to Change_Time to Display_Time
+        clockTester.ready();
         expectedOutput = "Time set to: Hour: 20 Minute: 30 Second: 15";
         assertEquals(expectedOutput,  clockTester.set(20,30,15));
         /** PASS **/
     }
-
+//
     @Test
     void transisition_1_2_1() {
         // Check Transition-1-2-1, From Display_Time to Display_Date to Display_Time
         clockTester.ready();
         expectedOutput = "Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.changeMode());
+        /** PASS **/
+    }
+    @Test
+    void transisition_1_2_1() {
+        // Check Transition-1-2-1, From Display_Time to Display_Date to Display_Time
+        clockTester.ready();
         expectedOutput = "Hour: 0 Minute: 0 Second: 0";
         assertEquals(expectedOutput,  clockTester.changeMode());
         /** PASS **/
     }
-
+//
+    @Test
+    void transisition_1_2_4() {
+        // Check Transition-1-2-4, From Display_Time to Display_Date to Change_Date
+        clockTester.ready();
+        expectedOutput = "Date ready to be set!";
+        assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
     @Test
     void transisition_1_2_4() {
         // Check Transition-1-2-4, From Display_Time to Display_Date to Change_Date
         clockTester.ready();
         expectedOutput = "Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.changeMode());
-        expectedOutput = "Date ready to be set!";
-        assertEquals(expectedOutput,  clockTester.ready());
         /** PASS **/
     }
-
+//
     @Test
     void transition_2_4_2() {
         // Check Transition-2-4-2, From Display_Date to Change_Date to Display_Date
         clockTester.ready();
         expectedOutput = "Date ready to be set!";
         assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
+    @Test
+    void transition_2_4_2() {
+        // Check Transition-2-4-2, From Display_Date to Change_Date to Display_Date
+        clockTester.ready();
         expectedOutput = "Date set to: Year: 2024 Month: 1 Day: 6";
         assertEquals(expectedOutput,  clockTester.set(2024,1,6));
         /** PASS **/
     }
-
+//
     @Test
     void transisition_2_1_2() {
         // Check Transition-2-1-2, From Display_Date to Display_Time to Display_Date
         clockTester.ready();
         expectedOutput = "Hour: 0 Minute: 0 Second: 0";
         assertEquals(expectedOutput,  clockTester.changeMode());
+        /** PASS **/
+    }
+    @Test
+    void transisition_2_1_2() {
+        // Check Transition-2-1-2, From Display_Date to Display_Time to Display_Date
+        clockTester.ready();
         expectedOutput = "Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.changeMode());
         /** PASS **/
     }
-
+//
+    @Test
+    void transisition_2_1_3() {
+        // Check Transition-2-1-3, From Display_Date to Display_Time to Change_Time
+        clockTester.ready();
+        expectedOutput = "Time ready to be set!";
+        assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
     @Test
     void transisition_2_1_3() {
         // Check Transition-2-1-3, From Display_Date to Display_Time to Change_Time
         clockTester.ready();
         expectedOutput = "Hour: 0 Minute: 0 Second: 0";
         assertEquals(expectedOutput,  clockTester.changeMode());
-        expectedOutput = "Time ready to be set!";
-        assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
+//
+    @Test
+    void transisition_3_1_3() {
+        // Check Transition-3-1-3, From Change_Time to Display_Time to Change_Time
+        clockTester.ready();
+        expectedOutput = "Time set to: Hour: 12 Minute: 5 Second: 30";
+        assertEquals(expectedOutput,  clockTester.set(12, 5, 30));
         /** PASS **/
     }
 
@@ -191,45 +299,62 @@ class ClockTest {
     void transisition_3_1_3() {
         // Check Transition-3-1-3, From Change_Time to Display_Time to Change_Time
         clockTester.ready();
-        expectedOutput = "Time set to: Hour: 12 Minute: 5 Second: 30";
-        assertEquals(expectedOutput,  clockTester.set(12, 5, 30));
         expectedOutput = "Time ready to be set!";
         assertEquals(expectedOutput,  clockTester.ready());
         /** PASS **/
     }
-
+//
     @Test
     void transisition_3_1_2() {
         // Check Transition-3-1-3, From Change_Time to Display_Time to Change_Time
         clockTester.ready();
         expectedOutput = "Time set to: Hour: 12 Minute: 5 Second: 30";
         assertEquals(expectedOutput,  clockTester.set(12, 5, 30));
+        /** PASS **/
+    }
+    @Test
+    void transisition_3_1_2() {
+        // Check Transition-3-1-3, From Change_Time to Display_Time to Change_Time
+        clockTester.ready();
         expectedOutput = "Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.changeMode());
         /** PASS **/
     }
-
+//
+    @Test
+    void transisition_4_2_4() {
+        // Check Transition-4-2-4, From Change_Date to Display_Date to Change_Date
+        clockTester.ready();
+        expectedOutput = "Date ready to be set!";
+        assertEquals(expectedOutput,  clockTester.ready());
+        /** PASS **/
+    }
     @Test
     void transisition_4_2_4() {
         // Check Transition-4-2-4, From Change_Date to Display_Date to Change_Date
         clockTester.ready();
         expectedOutput = "Date set to: Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.set(2024, 1, 1));
-        expectedOutput = "Date ready to be set!";
-        assertEquals(expectedOutput,  clockTester.ready());
         /** PASS **/
     }
-
+//
+    @Test
+    void transisition_4_2_1() {
+        // Check Transition-4-2-1, From Change_Date to Display_Date to Display_Time
+        clockTester.ready();
+        expectedOutput = "Hour: 0 Minute: 0 Second: 0";
+        assertEquals(expectedOutput,  clockTester.changeMode());
+        /** PASS **/
+    }
     @Test
     void transisition_4_2_1() {
         // Check Transition-4-2-1, From Change_Date to Display_Date to Display_Time
         clockTester.ready();
         expectedOutput = "Date set to: Year: 2024 Month: 1 Day: 1";
         assertEquals(expectedOutput,  clockTester.set(2024, 1, 1));
-        expectedOutput = "Hour: 0 Minute: 0 Second: 0";
-        assertEquals(expectedOutput,  clockTester.changeMode());
         /** PASS **/
     }
+//
     @Test
     void illegalTransition1() {
         // Check illegal Transition, Set() at Display_Date
